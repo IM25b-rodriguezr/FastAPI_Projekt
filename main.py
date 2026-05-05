dd89ead0e40cada142cd3436e2214d99875f1c8a
im25b-starmanst <starmanst@bzz.ch>
2026-05-05 14:51:54 +0200

    FastAPI Base

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# ....
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/test")
+def read_wuh():
+    return {"Test": "test"}
